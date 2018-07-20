e3758471b60620f401bcfe8ed5e323aaa28d8781
Jeremy Carter <Jeremy@JeremyCarter.ca>
2018-07-19 23:28:20 -0400

    update script

--- hpserver.py
+++ hpserver.py
@@ -3,6 +3,10 @@ import hpserver
 # Instantiate a new Server.
 s = hpserver.Server("serverconf.toml")
 
+# Print the contents of the Server instance.
+print("server contents:")
+print(dir(s))
+
 # Listen on the address that is in the serverconf.toml file.
 s.listen()
 
